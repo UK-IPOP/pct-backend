3478d46b73a0fa30e39d56a25ca899e987664db3
Nick Anthony <nanthony007@gmail.com>
2021-01-20 13:08:14 -0500

    Modify to refer to uk-ipop org

--- main.py
+++ main.py
@@ -18,18 +18,18 @@ g = Github(token)
 async def home(track: str, level: str):
     # setup
 
-    repo = g.get_repo("nanthony007/pct-decision-tool")
+    repo = g.get_repo("UK-IPOP/pct-decision-tool")
 
     # get current data inside file
     # is an array
     data = requests.get(
-        "https://raw.githubusercontent.com/nanthony007/pct-decision-tool/main/data/records.json"
+        "https://raw.githubusercontent.com/uk-ipop/pct-decision-tool/main/data/records.json"
     ).json()
 
     # get sha val from last commit
     sha_val = (
         requests.get(
-            "https://api.github.com/repos/nanthony007/pct-decision-tool/contents/data/records.json"
+            "https://api.github.com/repos/uk-ipop/pct-decision-tool/contents/data/records.json"
         )
         .json()
         .get("sha")
